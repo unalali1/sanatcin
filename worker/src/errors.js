36b61e00d8de889f71e5813f7fb217ff2543600a
Ali Unal <unalali@gmail.com>
2026-09-13 11:00:40 +0800

    Fix source extraction error classification

--- worker/src/errors.js
+++ worker/src/errors.js
@@ -1,9 +1,9 @@
 const rules = [
   ['DATE_INVALID', /tarih|Eski makale/i, 'date'],
   ['EDITORIAL_INVALID', /olgu|Editoryal|Çeviri|Türkçe|başlık|spot/i, 'editorial'],
-  ['IMAGE_INVALID', /görsel|image|HTTP 403|çözünürlük|oranı/i, 'image'],
   ['DUPLICATE', /benzer haber|known|daha önce/i, 'duplicate'],
   ['SOURCE_EXTRACTION', /Makale gövdesi|site navigasyonu|kısa duyuru|görsel altyazı/i, 'source'],
+  ['IMAGE_INVALID', /görsel|image|HTTP 403|çözünürlük|oranı/i, 'image'],
   ['SOURCE_NETWORK', /HTTP|fetch|abort|timeout/i, 'source']
 ];
 
