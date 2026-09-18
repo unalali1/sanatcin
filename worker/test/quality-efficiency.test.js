6abf497611901ce08880b77dc319baf4922e2eee
Ali Unal <unalali@gmail.com>
2026-09-18 11:02:17 +0800

    Test Güz Ortası Bayramı terminology

--- worker/test/quality-efficiency.test.js
+++ worker/test/quality-efficiency.test.js
@@ -60,6 +60,13 @@ test('açıklayıcı yabancı kültür-sanat adları doğal Türkçeye aktarıl�
   assert.doesNotMatch(translate, /Yerleşik karşılığı olmayan eser ve etkinlik adlarını uydurma biçimde çevirmeden özgün adıyla koru/);
 });
 
+test('Güz Ortası Bayramı terminolojisi deterministik olarak korunur', () => {
+  const translate = readFileSync(new URL('../src/translate.js', import.meta.url), 'utf8');
+  assert.match(translate, /Güz Ortası Bayramı/);
+  assert.match(translate, /Orta Sonbahar Bayramı/);
+  assert.match(translate, /Mid-Autumn Festival terminolojisini denetle/);
+});
+
 test('başlık üretimi katalog kalıbı ve klişe heyecan dilinden kaçınır', () => {
   const translate = readFileSync(new URL('../src/translate.js', import.meta.url), 'utf8');
   assert.match(translate, /yer adı \+ iki isimden oluşan katalog kalıbına bırakma/);
